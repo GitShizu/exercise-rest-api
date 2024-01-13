0f723c5f61063eb00f1bd22295ef34bcc3308e34
Alex F. <virex.dr@gmail.com>
2024-01-13 21:41:53 +0100

    Feat - all methods complete and working

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ const checkSingleResource = (resourceType, req, res) => {
 
 const isResourceValid = (resourceProps, newResource) => {
     let isValid = true;
-    isValid &= Object.keys(newResource).length >= 3 && Object.keys(newResource).length <=5;
+    isValid &= Object.keys(newResource).length >= 3 && Object.keys(newResource).length <= 5;
     if (isValid) {
         resourceProps.forEach((prop) => {
             isValid &= newResource[prop] !== undefined;
@@ -57,7 +57,7 @@ const isResourceValid = (resourceProps, newResource) => {
 }
 
 app.listen(3000, () => {
-    console.log('Server active - port 3000');
+    console.log('Server running - listening port 3000');
 })
 
 app.use(express.json());
@@ -69,18 +69,12 @@ app.get('/musicians', (req, res) => {
     res.send(resourceList);
 })
 
-//VIEW RESOURCE BY ID
-app.get('/musicians/:id', (req, res) => {
-    const resource = checkSingleResource('musicians', req, res)[0];
-    res.send(resource);
-})
-
 //ADD NEW RESOURCE
 app.post('/musicians', (req, res) => {
     const newResource = req.body;
     const resourceProps = ["name", "last_name", "occupation"]
-    let validResource = isResourceValid(resourceProps, newResource);
-    if(!validResource){
+    const isValid = isResourceValid(resourceProps, newResource);
+    if (!isValid) {
         res.status(400).send(`musicians must include ${resourceProps}. Up to 2 additional properties can be added (max. 5 total)`)
         return;
     }
@@ -91,11 +85,53 @@ app.post('/musicians', (req, res) => {
     res.send(resourceList);
 })
 
+//VIEW RESOURCE BY ID
+app.get('/musicians/:id', (req, res) => {
+    const resource = checkSingleResource('musicians', req, res)[0];
+    res.send(resource);
+})
+
 //DELETE RESOURCE BY ID
-app.delete('/musicians/:id', (req,res)=>{
-    const indexToDelete = checkSingleResource('musicians', req,res)[1];
+app.delete('/musicians/:id', (req, res) => {
+    const indexToDelete = checkSingleResource('musicians', req, res)[1];
     const resourceList = readResources('musicians');
     resourceList.splice(indexToDelete, 1);
     writeResources('musicians', resourceList);
+    res.send(resourceList);
+})
+
+//REPLACE RESOURCE BY ID
+app.put('/musicians/:id', (req, res) => {
+    const resourceProps = ["name", "last_name", "occupation"];
+    const newResource = req.body;
+    const isValid = isResourceValid(resourceProps, newResource);
+    if (!isValid) {
+        res.status(400).send(`musicians must include ${resourceProps}. Up to 2 additional properties can be included (max. 5 total)`)
+        return;
+    };
+    const indexToUpdate = checkSingleResource('musicians', req, res)[1];
+    const resourceList = readResources('musicians');
+    resourceList[indexToUpdate] = { ...newResource, id: Number(req.params.id) };
+    writeResources('musicians', resourceList)
+    res.send(resourceList);
+})
+
+//EDIT RESOURCE PROPERTIES BY ID
+app.patch('/musicians/:id', (req, res) => {
+    const resourceProps = ["name", "last_name", "occupation"]
+    const newProps = req.body;
+    const resourceList = readResources('musicians');
+    const indexToUpdate = checkSingleResource('musicians', req, res)[1];
+    const newResource = { ...resourceList[indexToUpdate], ...newProps }
+    let isNewPropsValid = Object.keys(newProps).length <= 4;
+    resourceProps.forEach((prop)=>{
+        isNewPropsValid &= Object.keys(newResource).includes(prop);
+    })
+    if (!isNewPropsValid) {
+        res.status(400).send(`musicians must include ${resourceProps}. Up to 2 additional properties can be included (max. 5 total)`)
+        return;
+    };
+    resourceList[indexToUpdate] = { ...resourceList[indexToUpdate], ...newProps };
+    writeResources('musicians', resourceList)
     res.send(resourceList);
 })
