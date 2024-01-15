99d1aa65f25246affe5c6377836d386b9a0a2ec5
Alex F. <virex.dr@gmail.com>
2024-01-15 17:33:44 +0100

    Refactor - complete, everything works

--- index.js
+++ index.js
@@ -18,11 +18,10 @@ app.use(cors({
 
 // //VIEW RESOURCE LIST
 app.get('/musicians/', async (req, res) => {
-    
-    try{
+    try {
         const musicians = await Musician.find();
         res.send(musicians)
-    }catch(e){
+    } catch (e) {
         res.status(500).send(e.message);
     }
 })
@@ -30,75 +29,51 @@ app.get('/musicians/', async (req, res) => {
 
 //ADD NEW RESOURCE
 app.post('/musicians/', async (req, res) => {
-    try{
-        const newResource = req.body;
-        const musician = await Musician.create(newResource);
+    try {
+        const musician = await Musician.create(req.body);
         res.send(musician);
-    }catch(e){
+    } catch (e) {
         res.status(400).send(e.message)
     }
 })
 
 // //VIEW RESOURCE BY ID
-// app.get(`/${resourceType}/:id`, (req, res) => {
-//     const resource = checkSingleResource(resourceType, req, res)[0];
-//     res.send(resource);
-// })
+app.get('/musicians/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const musician = await Musician.findById(id);
+        res.send(musician)
+    } catch (e) {
+        res.status(404).send(e.message);
+    }
+})
 
 // //DELETE RESOURCE BY ID
-// app.delete(`/${resourceType}/:id`, (req, res) => {
-//     const indexToDelete = checkSingleResource(resourceType, req, res)[1];
-//     const resourceList = readResources(resourceType);
-//     resourceList.splice(indexToDelete, 1);
-//     writeResources(resourceType, resourceList);
-//     res.send(resourceList);
-// })
+app.delete(`/musicians/:id`, async (req, res) => {
+    try {
+        const { id } = req.params
+        await Musician.findByIdAndDelete(id);
+        res.send(`Musician with ID ${id} successfully removed`)
+    } catch (e) {
+        res.status(404).send(e.message);
+    }
+})
 
-// //REPLACE RESOURCE BY ID
-// app.put(`/${resourceType}/:id`, (req, res) => {
-//     const newResource = req.body;
-//     const isValid = isResourceValid(resourceProps, newResource);
-//     if (!isValid) {
-//         res.status(400).send(`${resourceType} must include ${resourceProps}. Up to 2 additional properties can be included (optional)`)
-//         return;
-//     };
-//     const indexToUpdate = checkSingleResource(resourceType, req, res)[1];
-//     const resourceList = readResources(resourceType);
-//     resourceList[indexToUpdate] = { ...newResource, id: Number(req.params.id) };
-//     writeResources(resourceType, resourceList)
-//     res.send(resourceList);
-// })
-
-// //EDIT RESOURCE PROPERTIES BY ID
-// app.patch(`/${resourceType}/:id`, (req, res) => {
-//     const newProps = req.body;
-//     if (Object.keys(newProps).length > resourceProps.length + 1) {
-//         res.status(400).send(`Up to ${resourceProps.length + 1} properties can be edited. To edit all properties use the replace method.`)
-//         return;
-//     }
-//     if (Object.keys(newProps).includes('id')) {
-//         res.status(400).send('the "id" property is read-only')
-//         return;
-//     }
-//     const resourceList = readResources(resourceType);
-//     const indexToUpdate = checkSingleResource(resourceType, req, res)[1];
-//     const newResource = { ...resourceList[indexToUpdate], ...newProps }
-//     let isNewPropsValid = true;
-//     resourceProps.forEach((prop) => {
-//         isNewPropsValid &= Object.keys(newResource).includes(prop);
-//     })
-//     if (!isNewPropsValid) {
-//         res.status(400).send(`${resourceType} must include ${resourceProps}. Up to 2 additional properties can be included (optional)`)
-//         return;
-//     };
-//     resourceList[indexToUpdate] = { ...resourceList[indexToUpdate], ...newResource };
-//     writeResources(resourceType, resourceList)
-//     res.send(resourceList);
-// })
+// //EDIT/REPLACE RESOURCE BY ID
+app.patch(`/musicians/:id`, async (req, res) => {
+    try {
+        const { id } = req.params;
+        await Musician.findByIdAndUpdate(id, req.body);
+        const musician = await Musician.findById(id);
+        res.send(musician);
+    } catch (e) {
+        res.status(404).send(e.message)
+    }
+})
 
 //ENDPOINTS
 mongoose.connect(MONGO_URI)
-    .then(()=> {
+    .then(() => {
         console.log('Successfully connected to MongoDB');
         app.listen(3000, () => {
             console.log('Server running - listening at port 3000');
